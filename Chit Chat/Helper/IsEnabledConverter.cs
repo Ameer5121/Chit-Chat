bd57f21be7f717ada40f2b9a0256a918a1eadf10
Sound932 <shehadyamer@gmail.com>
2021-04-09 19:16:58 +0300

    Added a converter so that private chat doesn't work on your self.

---
+++ Chit Chat/Helper/IsEnabledConverter.cs
@@ -0,0 +1,30 @@
+﻿using ChitChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ChitChat.Helper
+{
+    class IsEnabledConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var currentUser = values[0] as UserModel;
+            var newUser = values[1] as string;
+            if(currentUser.DisplayName == newUser)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
