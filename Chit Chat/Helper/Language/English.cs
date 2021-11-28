dfff6106ccf35f078f05fb5f1749b48e22c6c32c
Amer Shehady <shehadyamer@gmail.com>
2021-11-28 14:51:33 +0200

    Added the corresponding classes for the keys

---
+++ Chit Chat/Helper/Language/English.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitChat.Helper.Language
+{
+    class English : ILanguage
+    {
+        public void ChangeLanguage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chit Chat/Helper/Language/Hebrew.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitChat.Helper.Language
+{
+    class Hebrew : ILanguage
+    {
+        public void ChangeLanguage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
