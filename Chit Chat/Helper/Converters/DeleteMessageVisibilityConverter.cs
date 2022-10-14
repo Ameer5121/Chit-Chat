3324f99dc37716e55337c80cbbaaf0c93cbd9d18
Amer Shehady <shehadyamer@gmail.com>
2022-10-14 17:27:22 +0300

    Added DeleteMessage Visiblity logic only for the messages that you have sent.

--- Chit Chat/Helper/Converters/DeleteMessageVisibilityConverter.cs
+++ Chit Chat/Helper/Converters/DeleteMessageVisibilityConverter.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 
 namespace ChitChat.Helper.Converters
 {
-    internal class DeleteMessageVisibilityConverter
+    internal class DeleteMessageVisibilityConverter : IMultiValueConverter
     {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string sender = values[0] as string;
+            string currentuser = values[1] as string;
+            if (sender != currentuser) return Visibility.Hidden;
+            return Visibility.Visible;
+        }
 
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
